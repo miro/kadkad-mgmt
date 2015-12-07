2c2943a286fe4ad145d8a2191ed79e5b71bfa0be
Miro Nieminen <miro.nieminen@futurice.com>
2015-12-07 20:46:52 +0200

    Add basic Gulpfile for SCSS build and Browsersync CSS injecting
    Applied from http://www.browsersync.io/docs/gulp/

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        proxy: 'localhost:9010'
+    });
+
+    gulp.watch("src/**/*.scss", ['sass']);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("src/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);

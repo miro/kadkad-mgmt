a6ae638a6c9f9f4b968fe8bcdffd5dedf4a0128f
Miro Nieminen <miro.nieminen@futurice.com>
2015-12-14 00:53:36 +0200

    Add stub implementation of apiService
    All the API interactions will be centralized in here

---
+++ src/apiService.js
@@ -0,0 +1,28 @@
+import request from 'superagent';
+import Promise from 'bluebird';
+
+const baseUrl = 'http://localhost:5000/api/v0/';
+
+
+export function uploadImage(imageFile) {
+  return new Promise((resolve, reject) => {
+
+    let formData = new FormData();
+    formData.append('imageFile', imageFile);
+
+    // HOX: superagent has currently issue with multipart form data,
+    // read more fromhttps://github.com/visionmedia/superagent/issues/746
+    request.post(baseUrl + 'image')
+      .send(formData)
+      .on('progress', function(e) {
+        console.log('Percentage done: ', e.percent);
+      })
+      .end((error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response.body);
+        }
+      });
+  });
+}

831e68496c1c6c08c7f8c64a13b7a519da59dcd5
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-03 14:00:38 +0200

    Set icons before header links

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -12,10 +12,21 @@ export default React.createClass({
     return <div className="header__wrapper pure-menu pure-menu-horizontal">
       <h1 className="header__title">Kajaaniskate.net</h1>
       <ul className="header__links">
-        <li><Link to={`/`} className="header__link"><i className="icon-koti"></i></Link></li>
-        <li><Link to={`/`} className="header__link">Muokkaa</Link></li>
-        <li><Link to={`/upload`} className="header__link">Uploadaa</Link></li>
-        <li><Link to={`/login`} className="header__link">Login</Link></li>
+        <li><Link to={`/`} className="header__link">
+          <i className="icon-koti"> Etusivu</i>
+        </Link></li>
+        <li><Link to={`/metadata`} className="header__link">
+          <i className="icon-tietokanta"> Paikat &amp; Ihmiset</i>
+        </Link></li>
+        <li><Link to={`/upload`} className="header__link">
+          <i className="icon-lataus2"> Uploadaa</i>
+        </Link></li>
+        <li><Link to={`/images`} className="header__link">
+          <i className="icon-kuvat"> Kuvat</i>
+        </Link></li>
+        <li><Link to={`/login`} className="header__link">
+          Login
+        </Link></li>
       </ul>
     </div>;
   }

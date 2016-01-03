b3a69007595dc856cd82e376fc98c402d0bc88c2
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-04 01:01:07 +0200

    Add simple paging to ImageEditView

--- src/containers/ImageEditView.jsx
+++ src/containers/ImageEditView.jsx
@@ -2,32 +2,65 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux'
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import classNames from 'classnames';
 
 import * as actionCreators from '../actions/action_creators';
 import {ImageList} from '../components/ImageList';
-import {Persons} from '../components/Persons';
-import {Spots} from '../components/Spots';
 
-
+// NOTE: do we need filters in here..?
 
 export const ImageEditView = React.createClass({
   mixins: [PureRenderMixin],
 
+  getInitialState: () => ({ page: 0, itemsInPage: 10 }),
+
+  // page related functions
+  turnNextPage() { this.changePage(true, this.props.images.length) },
+  turnPreviousPage() {this.changePage(false, this.props.images.length) },
+  changePage(turnForward, totalCount) {
+    let newPageNumber = (turnForward) ? this.state.page + 1 : this.state.page - 1;
+
+    // check that page doesn't underflow (is that a term?)
+    newPageNumber = (newPageNumber < 0) ? 0 : newPageNumber;
+
+    // check that page doesn't overflow
+    let lastIndexOnNewPage = newPageNumber * this.state.itemsInPage;
+    if (lastIndexOnNewPage <= totalCount) {
+      // NOTE this might have an error. too tired to check thoroughly now
+      this.setState({ page: newPageNumber });
+    }
+  },
 
   render: function() {
-    let { images, persons, spots, dispatch } = this.props;
+    const {images, persons, spots, dispatch} = this.props;
+    const {page, itemsInPage} = this.state
 
-    // TODO paging/filters
+    const startIndex = page * itemsInPage;
+    const endIndex = startIndex + itemsInPage;
+
+    const imagesOnThisPage = images.slice(startIndex, endIndex);
+    const totalImagesCount = images.length;
+
     return <div>
-        <h2>ImageEditView</h2>
+      <h2>Kuvat</h2>
 
-          images={images}
-          persons={persons}
-          spots={spots}
-          dispatch={dispatch}
-          {...bindActionCreators(actionCreators, dispatch)} />
+      <div className="paging__controls">
+        <button onClick={this.turnPreviousPage} className="paging__previous">
+          Edelliset
+        </button>
+        <p className="paging__state">
+          {startIndex+1} ... {endIndex} ({totalImagesCount} kuvaa yhteensä)
+        </p>
+        <button onClick={this.turnNextPage} className="paging__next">
+          Seuraavat
+        </button>
+      </div>
+
       <ImageList
+        images={imagesOnThisPage}
+        persons={persons}
+        spots={spots}
+        dispatch={dispatch}
+        {...bindActionCreators(actionCreators, dispatch)} />
     </div>;
   }
 });

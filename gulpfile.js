0caa480acd33fd76c072f6988db9872928bceae6
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-03 18:11:21 +0200

    Add cleanAssets & copyAssets tasks to gulpfile
    Also refactor the file to use 2-space indenting

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,42 @@ var browserSync             = require('browser-sync').create();
 var sass                    = require('gulp-sass');
 var autoprefixer            = require('gulp-autoprefixer');
 
-// TODO: autoprefixer!
-// TODO: task to copy assets to dist!
+var path                    = require('path');
+var del                     = require('del');
 
-// Static Server + watching scss/html files
+var cfg = {
+  assetsSrcDir: './src/assets',
+  assetsTargetDir: './dist/assets'
+};
+
+
+// Static Server + watching scss/html files for dev use
 gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    proxy: 'localhost:9010',
+    open: false
+  });
 
-    browserSync.init({
-        proxy: 'localhost:9010',
-        open: false
-    });
-
-    gulp.watch("src/**/*.scss", ['sass']);
+  gulp.watch('src/**/*.scss', ['sass']);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("src/**/*.scss")
-        .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(gulp.dest("dist/css"))
-        .pipe(browserSync.stream());
+  return gulp.src('src/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
 });
+
+gulp.task('cleanAssets', function (callback) {
+    del([path.join(cfg.assetsTargetDir, '**/*')], callback);
+});
+
+gulp.task('copyAssets', function() {
+    return gulp.src(path.join(cfg.assetsSrcDir, '**/*'))
+      .pipe(gulp.dest(cfg.assetsTargetDir));
+});
+
 
 gulp.task('default', ['serve']);

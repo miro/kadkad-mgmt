fa0d0a922b0f9fd429a9faa7069e3ccc721b906b
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-04 20:21:38 +0200

    Move ImageEditView paging state from state to new "appStore"

---
+++ src/reducers/appReducer.js
@@ -0,0 +1,48 @@
+import {List, Map} from 'immutable';
+
+export const defaultState = {
+  paging: { // state for the current page setup
+    imageEditView: {
+      itemsInPage: 10,
+      currentPage: 0
+    }
+  }
+}
+
+function setState(state, newState) {
+  return state.merge(newState);
+}
+
+function turnPage(state, viewName, turnForward, totalItemCount) {
+  console.log(viewName, turnForward, totalItemCount);
+  const pageState = state.getIn(['paging', viewName]);
+
+  let newPageNumber = (turnForward) ?
+    pageState.get('currentPage') + 1 :
+    pageState.get('currentPage') - 1;
+
+  // check that page doesn't underflow (is that a term?)
+  newPageNumber = (newPageNumber < 0) ? 0 : newPageNumber;
+
+  // check that page doesn't overflow
+  let lastIndexOnNewPage = newPageNumber * pageState.get('itemsInPage');
+
+  if (lastIndexOnNewPage <= totalItemCount) {
+    // NOTE this might have an error. too tired to check thoroughly now
+    return state.setIn(['paging', viewName, 'currentPage'], newPageNumber);
+  }
+  else {
+    return state;
+  }
+}
+
+export default function(state = Map(), action) {
+  switch (action.type) {
+  case 'SET_STATE':
+    return setState(state, action.state);
+  case 'TURN_PAGE':
+    return turnPage(state, action.viewName, action.turnForward, action.totalItemCount);
+  default:
+    return state;
+  }
+}

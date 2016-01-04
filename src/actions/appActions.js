6ee45fa122b6dbaf36d1b0af3443c3d7522c6b8c
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-04 23:28:54 +0200

    Remove SET_STATE from appActions - it is unnecessary in here
    Initial state of appStore will be created while initing it

--- src/actions/appActions.js
+++ src/actions/appActions.js
@@ -1,13 +1,4 @@
-export function setState(state) {
-  // TODO: general action creator for set state etc?
-  return {
-    type: 'SET_STATE',
-    state
-  };
-}
-
 export function turnPage(viewName, turnForward, totalItemCount) {
-  console.log(totalItemCount);
   return {
     type: 'TURN_PAGE',
     viewName,

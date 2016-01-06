265a01c2278edb231d1e5c83d5a36a280b700b87
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-07 00:03:03 +0200

    Add requireAuth prop for link items; hide them if user is not logged in

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -8,18 +8,24 @@ export const Header = React.createClass({
 
   linkItems: [
     { title: 'Etusivu', icon: 'koti', href: '/' },
-    { title: 'Paikat & Ihmiset', icon: 'tietokanta', href: '/metadata'},
-    { title: 'Uploadaa', icon: 'upload', href: '/upload' },
-    { title: 'Kuvat', icon: 'kuvat', href: '/images' },
-    { title: 'Login', icon: '', href: '/login'}
+    { title: 'Paikat & Ihmiset', icon: 'tietokanta', href: '/metadata', requiresAuth: true },
+    { title: 'Uploadaa', icon: 'upload', href: '/upload', requiresAuth: true },
+    { title: 'Kuvat', icon: 'kuvat', href: '/images', requiresAuth: true },
+    { title: 'Login', icon: '', href: '/login'} // TODO move to home page or something
   ],
 
   render: function() {
-    const links = this.linkItems.map(link => <li key={link.href}>
-      <Link to={link.href} className="header__link">
-        <i className={'icon-'+link.icon}></i> {link.title}
-      </Link>
-    </li>);
+    const user = this.props.user.toJS();
+
+    const links = this.linkItems.map(link => {
+      if (!link.requiresAuth || (link.requiresAuth && user.loggedIn)) {
+        return <li key={link.href}>
+          <Link to={link.href} className="header__link">
+            <i className={'icon-'+link.icon}></i> {link.title}
+          </Link>
+        </li>;
+      }
+    });
 
     return <div className="header__wrapper pure-menu pure-menu-horizontal">
       <h1 className="header__title">Kajaaniskate.net</h1>
@@ -30,4 +36,6 @@ export const Header = React.createClass({
   }
 });
 
-export const HeaderContainer = connect()(Header);
+export const HeaderContainer = connect(state => ({
+  user: state.app.get('user')
+}))(Header);

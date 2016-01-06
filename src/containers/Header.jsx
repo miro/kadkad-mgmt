6a9216f57e155b15871bcfdf882fb70361387218
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-06 23:52:33 +0200

    Generate Header link items programmatically

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -6,25 +6,25 @@ import {Link} from 'react-router';
 export const Header = React.createClass({
   mixins: [PureRenderMixin],
 
+  linkItems: [
+    { title: 'Etusivu', icon: 'koti', href: '/' },
+    { title: 'Paikat & Ihmiset', icon: 'tietokanta', href: '/metadata'},
+    { title: 'Uploadaa', icon: 'upload', href: '/upload' },
+    { title: 'Kuvat', icon: 'kuvat', href: '/images' },
+    { title: 'Login', icon: '', href: '/login'}
+  ],
+
   render: function() {
+    const links = this.linkItems.map(link => <li key={link.href}>
+      <Link to={link.href} className="header__link">
+        <i className={'icon-'+link.icon}></i> {link.title}
+      </Link>
+    </li>);
+
     return <div className="header__wrapper pure-menu pure-menu-horizontal">
       <h1 className="header__title">Kajaaniskate.net</h1>
       <ul className="header__links">
-        <li><Link to={`/`} className="header__link">
-          <i className="icon-koti"></i> Etusivu
-        </Link></li>
-        <li><Link to={`/metadata`} className="header__link">
-          <i className="icon-tietokanta"></i> Paikat &amp; Ihmiset
-        </Link></li>
-        <li><Link to={`/upload`} className="header__link">
-          <i className="icon-upload"></i> Uploadaa
-        </Link></li>
-        <li><Link to={`/images`} className="header__link">
-          <i className="icon-kuvat"></i> Kuvat
-        </Link></li>
-        <li><Link to={`/login`} className="header__link">
-          Login
-        </Link></li>
+        {links}
       </ul>
     </div>;
   }

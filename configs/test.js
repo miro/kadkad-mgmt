df6123afd0dcc192608064368292db96bf108a65
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-09 13:55:03 +0200

    Add test config which uses test API instance on Heroku

---
+++ configs/test.js
@@ -0,0 +1,7 @@
+module.exports = {
+  DAKDAK: {
+    storageUrl: JSON.stringify('https://storage.googleapis.com/dakdak-dev/'),
+    baseUrl: JSON.stringify('https://kajaaniskate-api-test.herokuapp.com'),
+    apiBaseUrl: JSON.stringify('https://kajaaniskate-api-test.herokuapp.com/api/v0/')
+  }
+};

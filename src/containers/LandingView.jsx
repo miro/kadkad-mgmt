8b57fa1ddf433301e6d2947db5f2e70180fabcd5
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-17 02:17:41 +0200

    Use Odometer in Landing page to show KPI data

--- src/containers/LandingView.jsx
+++ src/containers/LandingView.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux'
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+import * as app from '../actions/appActions';
 import {InvitationContainer} from './Invitation';
+import Odometer from '../components/Odometer';
 
 
 export const LandingView = React.createClass({
   mixins: [PureRenderMixin],
 
   render() {
+    const {kpi} = this.props;
+
     return <div>
         <h2>LandingView</h2>
         <p className="placeholder">Placeholder ländärille</p>
 
         <InvitationContainer />
+
+        <div className="kpi__wrapper">
+          <Odometer value={kpi.pixelCount} minDigits={12} />
+          <Odometer value={kpi.imageCount} minDigits={12} />
+        </div>
     </div>;
   }
 });
 
 function mapStateToProps(state) {
-  return {};
+  return {
+    kpi: state.app.getIn(['appState', 'kpi'])
+  };
 }
 
 export const LandingViewContainer = connect(

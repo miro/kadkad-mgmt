df75c389ade63f6a9fd35a0ac7c303c2abef62fd
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-29 22:50:44 +0200

    Add identical role-service as in dakdak-api
    "Original" can be seen in here https://github.com/miro/dakdak-api/blob/development/services/role.js

---
+++ src/services/role.js
@@ -0,0 +1,36 @@
+// # roleService
+//      direct copy of https://github.com/miro/dakdak-api/blob/development/services/role.js
+//      these must be kept in sync!
+import _ from 'lodash';
+
+export const UNREGISTERED = 'UNREGISTERED';
+export const USER = 'USER';
+export const EDITOR = 'EDITOR';
+export const ADMIN = 'ADMIN';
+
+const rolesConfig = [
+  UNREGISTERED,
+  USER,
+  EDITOR,
+  ADMIN
+];
+
+// Check if user is allowed to run stuff which are available only for requiredRole,
+// returns the result as boolean. Will fallback to false
+export function isAuthorized(userRole, requiredRole) {
+  const userRoleIndex = rolesConfig.indexOf(userRole);
+  const requiredRoleIndex = rolesConfig.indexOf(requiredRole);
+
+  if (userRoleIndex >= 0 && requiredRoleIndex >= 0) {
+    return (userRoleIndex >= requiredRoleIndex);
+  }
+  else if (userRoleIndex < 0 && requiredRoleIndex < 0) {
+    // user had unregistered/undefined role, but so was the required role -> return true
+    // NOTE: this might be again begging for errors..
+    return true;
+  }
+  else {
+    // otherwise, fallback to false
+    return false;
+  }
+}

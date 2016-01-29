2ec831743d5e7a5ff9ccfe7bdd8c6edbf1d58be0
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-30 00:29:31 +0200

    Add logout functionality to Header

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -3,24 +3,27 @@ import {connect} from 'react-redux';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {Link} from 'react-router';
 
+import {userLogout} from '../actions/appActions';
 import * as role from '../services/role';
 
 export const Header = React.createClass({
   mixins: [PureRenderMixin],
 
   linkItems: [
-    { title: 'Etusivu', icon: 'koti', href: '/' },
+    { title: '', icon: 'koti', href: '/' },
     { title: 'Paikat & Ihmiset', icon: 'tietokanta', href: '/metadata', requiresAuth: true, requiresRole: role.EDITOR },
     { title: 'Uploadaa', icon: 'upload', href: '/upload', requiresAuth: true },
     { title: 'Kuvat', icon: 'kuvat', href: '/images', requiresAuth: true }
   ],
 
+  onLogoutClick() {
+    this.props.dispatch(userLogout());
+  },
+
   render: function() {
     const user = this.props.user.toJS();
 
-    console.log(user);
-
-    const links = this.linkItems.map(link => {
+    let links = this.linkItems.map(link => {
       if (!link.requiresAuth || (link.requiresAuth && user.loggedIn) &&
          (!link.requiresRole || role.isAuthorized(user.profile.role, role.EDITOR))) {
 
@@ -31,6 +34,12 @@ export const Header = React.createClass({
         </li>;
       }
     });
+
+    if (user.loggedIn) {
+      links.push(<li key="logout-link" className="header__link" onClick={this.onLogoutClick}>
+        <i className="icon-nuoli-vasen"></i> <span className="link__title--always">Logout</span>
+      </li>);
+    }
 
     return <div className="header__wrapper pure-menu pure-menu-horizontal">
       <h1 className="header__title header__title--full">Kajaaniskate.net</h1>

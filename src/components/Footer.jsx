5db70e8bfef2001fc01ca81d0db8d17f5d6a3d1d
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-30 14:05:27 +0200

    Add some magic unicorn touch to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,15 +7,18 @@ export default React.createClass({
   render: function() {
     return <div className="footer__wrapper">
       <div className="footer__column">
-        <h3 className="footer__title">Kajaaniskate.net</h3>
+        <h3 className="footer__title">&nbsp;</h3>
         <p>2014-2016 <a href="https://github.com/miro/kadkad-mgmt/blob/development/LICENSE">Jotkut oikeudet pidätetään</a></p>
         <p><a href="https://github.com/miro/kadkad-mgmt">Haarukoi koodit <i className="icon-github"></i> GitHubista</a></p>
       </div>
 
       <div className="footer__column">
-        <h3 className="footer__title">&nbsp;</h3>
+        <h3 className="footer__title">Kajaaniskate.net</h3>
+        <p>Kysymyksiä? <a href="mailto:miro@kajaaniskate.net">miro@kajaaniskate.net</a></p>
         <p>Kehitystä tukemassa <a href="http://spiceprogram.org/">Spice Program</a></p>
-        <img id="chilicorn--logo" src="/assets/chilicorn.png" alt="Chilicorn!"/>
+        <a href="http://spiceprogram.org/">
+          <img id="chilicorn--logo" src="/assets/chilicorn.png" alt="Chilicorn!"/>
+        </a>
       </div>
     </div>
   }

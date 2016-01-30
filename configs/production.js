d5449043c36a1b3882a11b79bf1c0baa08c5546a
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-30 15:44:33 +0200

    Add NODE_ENV=production to get rid of Redux's warning about minified code

--- configs/production.js
+++ configs/production.js
@@ -3,5 +3,6 @@ module.exports = {
     storageUrl: JSON.stringify('https://storage.googleapis.com/kajaaniskate/'),
     baseUrl: JSON.stringify('https://kajaaniskate.herokuapp.com'),
     apiBaseUrl: JSON.stringify('https://kajaaniskate-api.herokuapp.com/api/v0/')
-  }
+  },
+  NODE_ENV: 'production'
 };
--- configs/test.js
+++ configs/test.js
@@ -3,5 +3,6 @@ module.exports = {
     storageUrl: JSON.stringify('https://storage.googleapis.com/dakdak-test/'),
     baseUrl: JSON.stringify('https://kajaaniskate-api-test.herokuapp.com'),
     apiBaseUrl: JSON.stringify('https://kajaaniskate-api-test.herokuapp.com/api/v0/')
-  }
+  },
+  NODE_ENV: 'production'
 };

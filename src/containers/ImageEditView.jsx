5a70c13ce43871c31bce71d8906a160f6edc1367
Miro Nieminen <miro.nieminen@futurice.com>
2016-02-14 10:40:08 +0900

    Refactor: separate PageControls into own container

--- src/containers/ImageEditView.jsx
+++ src/containers/ImageEditView.jsx
@@ -6,16 +6,16 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import * as modelActions from '../actions/modelActions';
 import * as appActions from '../actions/appActions';
 
+import {PageControlsContainer} from './PageControls';
 import {ImageList} from '../components/ImageList';
 
-// NOTE: do we need filters in here..?
+// TODO: show loading indicator instead of "no images, upload some" -text if first fetch isn't ready yet
+// TODO: add "back to top" -button
 
 const VIEW_NAME = 'imageEditView';
 
 export const ImageEditView = React.createClass({
   mixins: [PureRenderMixin],
-
-  getInitialState: () => ({ page: 0, itemsInPage: 10 }),
 
   componentDidMount() {
     this.props.dispatch(modelActions.getAllModels('images'));
@@ -23,46 +23,38 @@ export const ImageEditView = React.createClass({
     this.props.dispatch(modelActions.getAllModels('spots'));
   },
 
-  // page related functions
-  turnNextPage() { this.changePage(true, this.props.totalImagesCount) },
-  turnPreviousPage() {this.changePage(false, this.props.totalImagesCount) },
-  changePage(turnForward, totalCount) {
-    this.props.dispatch(appActions.turnPage(VIEW_NAME, turnForward, totalCount));
-  },
-
   render: function() {
     const {imagesOnThisPage, persons, spots, startIndex, endIndex, totalImagesCount, dispatch} = this.props;
 
-    const pagingControls = <div className="paging__controls">
-      <button onClick={this.turnPreviousPage} className="paging__previous btn-primary">
-        <i className="icon-nuoli-vasen"></i>
-      </button>
-      <p className="paging__state">
-        {startIndex+1} ... {endIndex} ({totalImagesCount} kuvaa yhteensä)
-      </p>
-      <button onClick={this.turnNextPage} className="paging__next btn-primary">
-        <i className="icon-nuoli-oikea"></i>
-      </button>
-    </div>;
+    if (imagesOnThisPage.length > 0) {
+      return <div>
+        <h2 className="view__title">
+          <i className="icon-kuvat"></i> Kuvien tietojen muokkaus
+        </h2>
 
+        <PageControlsContainer
+          viewName={VIEW_NAME}
+          totalItemCount={totalImagesCount}
+        />
 
-    return (imagesOnThisPage.length > 0) ?
-    <div>
-      <h2 className="view__title">
-        <i className="icon-kuvat"></i> Kuvien tietojen muokkaus
-      </h2>
+        <ImageList
+          images={imagesOnThisPage}
+          persons={persons}
+          spots={spots}
+          dispatch={dispatch}
+          {...bindActionCreators(modelActions, dispatch)} />
 
-      {pagingControls}
-      <ImageList
-        images={imagesOnThisPage}
-        persons={persons}
-        spots={spots}
-        dispatch={dispatch}
-        {...bindActionCreators(modelActions, dispatch)} />
-      {pagingControls}
-
-    </div> :
-    <p>Ei vielä yhtään kuvaa - uploadaa jotain!</p>
+        <PageControlsContainer
+          viewName={VIEW_NAME}
+          totalItemCount={totalImagesCount}
+        />
+      </div>;
+    }
+    else {
+      return <div>
+        <p>Ei vielä yhtään kuvaa - uploadaa jotain!</p>
+      </div>;
+    }
   }
 });
 

a3595d50d0737fa0928682f02a639ef84511def1
Miro Nieminen <miro.nieminen@futurice.com>
2016-02-18 12:14:51 +0900

    Bugfix to Image reel component generating

--- src/components/ImageReel.jsx
+++ src/components/ImageReel.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import Slider from 'react-slick';
+import {List} from 'immutable';
 
+import Slider from 'react-slick';
 import ImageHilight from './ImageHilight';
 import Spinner from './Spinner';
 
@@ -24,15 +25,18 @@ export default React.createClass({
   },
 
   render: function () {
-    const imagesDefault = [];
-    const images = this.props.images || imagesDefault;
 
-    // note: for some reason Slider requires that the element is wrapped with <div>
-    const imageComponents = images.map(image => <div key={image.id}><ImageHilight model={image} /></div>);
+    if (List.isList(this.props.images)) {
+      // note: for some reason Slider requires that the element is wrapped with <div>
+      const imageComponents = this.props.images
+        .toJS()
+        .map(image => <div key={image.id}><ImageHilight key={image.id} model={image} /></div>);
 
-    if (this.props.images) {
       return <div className="image-reel__wrapper">
-        <h3 className="card__purpose--image"><i className="icon-kuvat"></i> 10 viimeksi muokattua/l&auml;hetetty&auml; kuvaa</h3>
+        <h3 className="card__purpose--image">
+          <i className="icon-kuvat"></i> 10 viimeksi muokattua/l&auml;hetetty&auml; kuvaa
+        </h3>
+
         <Slider {...this.sliderSettings}>
           {imageComponents}
         </Slider>
